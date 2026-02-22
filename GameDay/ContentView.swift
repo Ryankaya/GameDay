42d1a9bce5361215a546b4e16a1cd999ae4d032d
htkaya07 <47039316+htkaya07@users.noreply.github.com>
2026-02-22 07:25:24 -0500

    Initial Commit

---
+++ GameDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameDay
+//
+//  Created by Ryan Kaya on 2/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameDay/GameDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDayApp.swift
+//  GameDay
+//
+//  Created by Ryan Kaya on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct GameDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
